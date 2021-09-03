450d3ad6b83e3a74a8c0f02d148aebb689632888
ruth <jamesruth449@gmail.com>
2021-09-02 20:28:55 -0700

    implementQRCode

---
+++ GeneratingQRCode/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GeneratingQRCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace GeneratingQRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult CreateQRCode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateQRCode(QRCodeModel qRCode)
+        {
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(qRCode.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            ViewBag.QrCodeUri = QrUri;
+            return View();
+        }
+    }
+
+    //Extension method to convert Bitmap to Byte Array
+    public static class BitmapExtension
+    {
+        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ GeneratingQRCode/Models/QRCodeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeneratingQRCode.Models
+{
+    public class QRCodeModel
+    {
+        [Display(Name = "Enter QRCode Text")]
+        public string QRCodeText { get; set; }
+
+    }
+}
